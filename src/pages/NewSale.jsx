dedda2aa0cbf78dc0231d6ffb998047ffda90eb5
DiogoRachid <di.rachid@gmail.com>
2026-04-01 20:05:45 -0300

    Update NewSale.jsx

--- src/pages/NewSale.jsx
+++ src/pages/NewSale.jsx
@@ -1,2 +1,3 @@
-const ComponentName = () => <div className="p-8"><h1>Página OK!</h1></div>;
-export default ComponentName;
+import React from 'react';
+const NewSale = () => <div className="p-8"><h1>Nova Venda OK!</h1></div>;
+export default NewSale;
