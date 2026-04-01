5e027c99b8d94432ac987823994f31b11d2772cc
DiogoRachid <di.rachid@gmail.com>
2026-04-01 20:05:20 -0300

    Update ProductForm.jsx

--- src/pages/ProductForm.jsx
+++ src/pages/ProductForm.jsx
@@ -1,2 +1,3 @@
-const ComponentName = () => <div className="p-8"><h1>Página OK!</h1></div>;
-export default ComponentName;
+import React from 'react';
+const ProductForm = () => <div className="p-8"><h1>Novo Produto OK!</h1></div>;
+export default ProductForm;
