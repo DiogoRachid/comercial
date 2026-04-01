e54889efb92209cd1aeebc7980185b78f6ab8f98
DiogoRachid <di.rachid@gmail.com>
2026-04-01 20:04:18 -0300

    Update Companies.jsx

--- src/pages/Companies.jsx
+++ src/pages/Companies.jsx
@@ -1,2 +1,12 @@
-const ComponentName = () => <div className="p-8"><h1>Página OK!</h1></div>;
-export default ComponentName;
+import React from 'react';
+
+const Companies = () => {
+  return (
+    <div className="p-8 space-y-4">
+      <h1 className="text-3xl font-bold text-gray-900">Empresas</h1>
+      <p>Empresas carregou OK!</p>
+    </div>
+  );
+};
+
+export default Companies;
